a30c6d8814013e047e41282ee85e6528de21658a
MasterAwesome <armu30@gmail.com>
2018-09-20 23:21:39 +0530

    sanders: add com.qualcomm.qti.ant to vendor
    Change-Id: I541bf637d99aa3285f325796dae2f46f4a63e0da
    Signed-off-by: MasterAwesome <armu30@gmail.com>

--- Android.mk
+++ Android.mk
@@ -30,6 +30,17 @@ LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.qti.ant@1.0
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES_32 := proprietary/lib/com.qualcomm.qti.ant@1.0_vendor.so
+LOCAL_SRC_FILES_64 := proprietary/lib64/com.qualcomm.qti.ant@1.0_vendor.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := QtiTelephonyService
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/app/QtiTelephonyService/QtiTelephonyService.apk
